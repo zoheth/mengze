bc556408ddf2154d21791f738a3ec4024ce3b46f
kaixuan <kaixuan@example.com>
2023-11-30 23:38:29 +0800

    temp

--- mengze/visible_surface_determination/edge_table.h
+++ mengze/visible_surface_determination/edge_table.h
@@ -8,11 +8,15 @@
 
 namespace mengze
 {
+	struct EdgePair;
+
 	struct Polygon
 	{
 		uint32_t id;
 		float a, b, c, d;
 		uint32_t num_y;
+		std::array<glm::vec3, 3> vertices;
+		EdgePair* p_edge_pair{nullptr};
 	};
 
 	struct Edge
@@ -36,14 +40,14 @@ namespace mengze
 	struct PolygonStorage
 	{
 		std::vector<std::list<Polygon>> polygon_table;
-		std::vector<std::list<Edge>> edge_table;
+		// std::vector<std::list<Edge>> edge_table;
 
 		PolygonStorage() = delete;
 
 		explicit PolygonStorage(uint32_t y)
 		{
 			polygon_table.resize(y);
-			edge_table.resize(y);
+			// edge_table.resize(y);
 		}
 
 		void add_triangle(const Triangle& triangle, uint32_t index)
@@ -69,25 +73,69 @@ namespace mengze
 			auto y = static_cast<uint32_t>(vertices[0].y);
 			auto max_y = static_cast<uint32_t>(vertices[2].y);
 			polygon.num_y = max_y - y + 1;
+			polygon.vertices = vertices;
 
 			polygon_table[y].push_back(polygon);
-
-
-			edge_table[y].emplace_back(vertices[0], vertices[1], index);
-			edge_table[y].emplace_back(vertices[0], vertices[2], index);
-			edge_table[static_cast<uint32_t>(vertices[1].y)].emplace_back(vertices[1], vertices[2], index);
 
 		}
 	};
 
+	struct EdgePair
+	{
+		Edge left;
+		Edge right;
+		float z_left;
+		float delta_z_x;
+		float delta_z_y;
+		std::array<glm::vec3, 3> vertices;
+		EdgePair(const Polygon& polygon)
+		{
+			vertices = polygon.vertices;
 
+			std::sort(vertices.begin(), vertices.end(), [](const glm::vec3& a, const glm::vec3& b)
+				{
+					return a.y < b.y;
+				});
+
+			if(vertices[1].x < vertices[2].x)
+			{
+				left = Edge(vertices[0], vertices[1], polygon.id);
+				right = Edge(vertices[0], vertices[2], polygon.id);
+			}
+			else
+			{
+				left = Edge(vertices[0], vertices[2], polygon.id);
+				right = Edge(vertices[0], vertices[1], polygon.id);
+			}
+
+			z_left = -polygon.d - polygon.a * vertices[0].x - polygon.b * vertices[0].y;
+			delta_z_x = -polygon.a / polygon.c;
+			delta_z_y = -polygon.b / polygon.c;
+		}
+		// false to delete
+		bool update()
+		{
+			left.num_y--;
+			right.num_y--;
+
+			if(left.num_y <= 0)
+			{
+				left=Edge(vertices[1], vertices[2], left.polygon_id);
+			}
+			else if(right.num_y <= 0)
+			{
+				right= Edge(vertices[1], vertices[2], left.polygon_id);
+			}
+
+		}
+	};
 
 	struct ActiveEdge
 	{
 		std::list<Polygon> polygons;
-		std::list<Edge> edges;
+		std::list<EdgePair> edge_pairs;
 
-		uint32_t cur_y=-1;
+		uint32_t cur_y = -1;
 
 		void update(const PolygonStorage& polygon_storage)
 		{
@@ -95,15 +143,21 @@ namespace mengze
 			for (auto& polygon : polygons)
 			{
 				polygon.num_y--;
-				if(polygon.num_y<=0)
+				if (polygon.num_y <= 0)
 				{
 					polygons.remove(polygon);
 				}
+				else
+				{
+					polygon.p_edge_pair->update();
+				}
+				
 			}
 
 			for (auto polygen : polygon_storage.polygon_table[cur_y])
 			{
 				polygons.push_back(polygen);
+				edge_pairs.emplace_back(polygen);
 			}
 
 		}
